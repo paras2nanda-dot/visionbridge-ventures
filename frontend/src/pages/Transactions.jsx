f71c5e33c8a4bdbe16701264a9bbb3c50a1092d7
paras2nanda-dot <paras2nanda@gmail.com>
2026-04-12 02:14:41 +0530

    Fix: Removed stray backslash in transactions.jsx

--- frontend/src/pages/Transactions.jsx
+++ frontend/src/pages/Transactions.jsx
@@ -1,4 +1,4 @@
-\/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import api from '../services/api'; 
 import { toast } from 'react-toastify'; 
